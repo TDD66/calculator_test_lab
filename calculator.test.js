094cd5e2e9614ce7d3a6786f12ce99097c698563
Tadiwanashe Dzvoti <tadiwadzvoti@hotmail.com>
2024-03-12 13:34:40 +0000

    Finished unit tests for sum.

--- calculator.test.js
+++ calculator.test.js
@@ -16,16 +16,28 @@ describe('sum', () => {
     expect(actual).toBe(expected);
   });
 
-  test.skip('can add two large positive numbers', () => { 
-    
+  test('can add two large positive numbers', () => { 
+    expected = 123213135462553;
+    actual = sum(12321312, 123213123141241);
+    expect(actual).toBe(expected);
   });
 
-  test.skip('can add two negative numbers', () => {
-    
+  test('can add two negative numbers', () => {
+    expected = -10;
+    actual = sum(-6, -4);
+    expect(actual).toBe(expected);
   });
 
-  test.skip('can add zero', () => {
-    
+  test('can add zero', () => {
+    expected = 5;
+    actual = sum(0, 5);
+    expect(actual).toBe(expected);
+  });
+
+  test('can add positive number to negative number', () => {
+    expected = 10;
+    actual = sum(20, -10);
+    expect(actual).toBe(expected);
   });
 
 });
